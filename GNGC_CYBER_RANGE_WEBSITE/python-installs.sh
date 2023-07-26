9ef24d381c3ffe8eb54e37dfa22a420b3c4d8d4c
Fox2low <77130209+Fox2low@users.noreply.github.com>
2023-07-26 01:31:56 +0000

    modified:   GNGC_CYBER_RANGE_WEBSITE/python-installs.sh

--- GNGC_CYBER_RANGE_WEBSITE/python-installs.sh
+++ GNGC_CYBER_RANGE_WEBSITE/python-installs.sh
@@ -1,6 +1,6 @@
 echo "This is a python import install for all dependencies"
 echo ""
-read -p "Continuing in 5 seconds or press enter to tontinue" -t 5
+read -p "Continuing in 5 seconds or press enter to continue" -t 5
 echo ""
 echo "\n Continuing ...."
 pip install flask
