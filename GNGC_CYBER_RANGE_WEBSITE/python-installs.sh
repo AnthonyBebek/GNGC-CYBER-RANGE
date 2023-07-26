8e8ab4e5b3eb41ddcf4a501a0f9a1b3f1101c3a9
Fox2low <77130209+Fox2low@users.noreply.github.com>
2023-07-26 01:30:44 +0000

    new file:   GNGC_CYBER_RANGE_WEBSITE/python-installs.sh

---
+++ GNGC_CYBER_RANGE_WEBSITE/python-installs.sh
@@ -0,0 +1,9 @@
+echo "This is a python import install for all dependencies"
+echo ""
+read -p "Continuing in 5 seconds or press enter to tontinue" -t 5
+echo ""
+echo "\n Continuing ...."
+pip install flask
+pip install werkzeug
+pip install sqlalchemy
+pip install re
