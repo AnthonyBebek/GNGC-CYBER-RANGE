753cd3714a2a7dcdbae2e4678b2812d125dfbad4
omvedanti <140563395+omvedanti@users.noreply.github.com>
2023-07-26 22:59:54 +0000

    new file:   GNGC_CYBER_RANGE_WEBSITE/models.py 	deleted:    GNGC_CYBER_RANGE_WEBSITE/TEMPLATES/index.html

---
+++ GNGC_CYBER_RANGE_WEBSITE/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import (String,
+Column,
+Integer,
+DateTime, 
+ForeignKey,
+UniqueConstraint 
+)
+from sqlalchemy.ext.associationproxy import association_proxy
+from database import *
+from sqlalchemy.orm import relationship
+
