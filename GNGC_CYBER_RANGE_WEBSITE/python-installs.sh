a952db1d2c17cfa251a4d57fab33ad8444675e76
Fox2low <77130209+Fox2low@users.noreply.github.com>
2023-07-26 07:35:44 +0000

    modified:   GNGC_CYBER_RANGE_WEBSITE/python-installs.sh

--- GNGC_CYBER_RANGE_WEBSITE/python-installs.sh
+++ GNGC_CYBER_RANGE_WEBSITE/python-installs.sh
@@ -1,11 +1,46 @@
-echo "This is a python import install for all dependencies"
-echo ""
-read -p "Continuing in 5 seconds or press enter to continue" -t 5
-echo ""
-echo "\n Continuing ...."
-pip3 install flask
-pip3 install flask_login
-pip3 install werkzeug
-pip3 install sqlalchemy
-pip3 install re
-pip3 cache purge
+#!/bin/bash
+
+end_message(){
+    echo ""
+    echo "If there was an error then it's recommended to restart the script and agree to update installers"
+    echo ""
+}
+
+update_installers() {
+    echo "Updating Installers..."
+    sudo apt update
+    pip3 install --upgrade
+}
+
+install_python_dependencies() {
+    echo "Installing Python dependencies..."
+    pip3 install flask
+    pip3 install flask_login
+    pip3 install werkzeug
+    pip3 install sqlalchemy
+    pip3 install re
+    pip3 cache purge
+    end_message
+}
+
+install_other_dependencies() {
+    echo "Installing other dependencies..."
+    sudo apt install mariadb-server
+    end_message
+}
+
+read -p "Do you want to update apt and pip? (Recommended) (Y/N): " python_choice
+if [[ "$python_choice" =~ ^[Yy]$ ]]; then
+    update_installers
+fi
+
+read -p "Do you want to install Python dependencies? (Y/N): " python_choice
+if [[ "$python_choice" =~ ^[Yy]$ ]]; then
+    install_python_dependencies
+fi
+
+read -p "Do you want to install other dependencies? (Y/N): " other_choice
+if [[ "$other_choice" =~ ^[Yy]$ ]]; then
+    install_other_dependencies
+fi
+echo "Done!"
