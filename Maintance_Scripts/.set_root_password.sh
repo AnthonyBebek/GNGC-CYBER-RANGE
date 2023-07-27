b14202bd201662b9357448d107ae5a82f86123e3
Fox2low <77130209+Fox2low@users.noreply.github.com>
2023-07-27 01:56:41 +0000

    deleted:    Maintance_Scripts/.my.cnf 	deleted:    Maintance_Scripts/.set_root_password.sh 	modified:   Maintance_Scripts/setupDB.sh

--- Maintance_Scripts/.set_root_password.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-root_password="GNGC-MYSQL"
-
-# Set the root password for MySQL using mysqladmin
-mysqladmin -u root password "$root_password"
-
-echo "Root password set successfully."
--- Maintance_Scripts/setupDB.sh
+++ Maintance_Scripts/setupDB.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-current_user=$(whoami)
-new_password="pass"
+set_root_password() {
+    mysqladmin -u root password "GNGC-MYSQL"
+    echo "Root password set successfully."
+}
 
-echo "Creating new user: $current_user"
-echo "New password: $new_password"
+create_or_update_user() {
+    local current_user=$(whoami)
+    local new_password="pass"
 
-bash ./.set_root_password.sh
+    echo "Creating new user: $current_user"
+    echo "New password: $new_password"
 
-user_exists=$(mysql --defaults-extra-file=<(echo -e "[client]\nuser=root\npassword=your_new_root_password") -N -s -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$current_user' AND host = 'localhost');")
-
-if [ "$user_exists" -eq 1 ]; then
-    echo "User '$current_user' already exists. Deleting..."
-    mysql --defaults-extra-file=<(echo -e "[client]\nuser=root\npassword=your_new_root_password") -e "DROP USER '$current_user'@'localhost';"
-fi
-
-tmp_cnf=$(mktemp)
-echo "[client]" >> "$tmp_cnf"
-echo "user=root" >> "$tmp_cnf"
-echo "password=your_new_root_password" >> "$tmp_cnf"
-
-mysql --defaults-extra-file="$tmp_cnf" <<MYSQL_SCRIPT
-CREATE USER '$current_user'@'localhost' IDENTIFIED BY '$new_password';
+    mysql --user=root --password=GNGC-MYSQL <<MYSQL_SCRIPT
+CREATE OR REPLACE USER '$current_user'@'localhost' IDENTIFIED BY '$new_password';
 GRANT ALL PRIVILEGES ON *.* TO '$current_user'@'localhost';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
-rm "$tmp_cnf"
+    echo ""
+    echo "New account has been created or updated!"
+}
 
-echo ""
-echo "New account has been created!"
+set_root_password
+create_or_update_user
