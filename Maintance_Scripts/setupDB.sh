5dc1ad16ff4523acf7e40b5794179ee6b34a23d6
Fox2low <77130209+Fox2low@users.noreply.github.com>
2023-07-27 10:07:39 +0000

    modified:   Maintance_Scripts/setupDB.sh

--- Maintance_Scripts/setupDB.sh
+++ Maintance_Scripts/setupDB.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set_root_password() {
-    mysqladmin -u root password "GNGC-MYSQL"
+    mysqladmin -u root --password="GNGC-MYSQL" password "GNGC-MYSQL"
     echo "Root password set successfully."
 }
 
@@ -12,16 +12,15 @@ create_or_update_user() {
     echo "Creating new user: $current_user"
     echo "New password: $new_password"
 
-    mysql --user=root --password=GNGC-MYSQL --socket=/path/to/mysql.sock <<MYSQL_SCRIPT
-CREATE OR REPLACE USER '$current_user'@'localhost' IDENTIFIED BY '$new_password';
-GRANT ALL PRIVILEGES ON *.* TO '$current_user'@'localhost';
-FLUSH PRIVILEGES;
-MYSQL_SCRIPT
+    mysql --user=root --password=GNGC-MYSQL --socket=/var/run/mysqld/mysqld.sock -e "
+        CREATE OR REPLACE USER '$current_user'@'localhost' IDENTIFIED BY '$new_password';
+        GRANT ALL PRIVILEGES ON *.* TO '$current_user'@'localhost';
+        FLUSH PRIVILEGES;
+    "
 
     echo ""
     echo "New account has been created or updated!"
 }
-
 
 set_root_password
 create_or_update_user
