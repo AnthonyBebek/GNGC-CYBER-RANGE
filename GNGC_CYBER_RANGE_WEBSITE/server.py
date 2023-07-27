3ea5a983b764f684bef089447ceba15aa5716152
omvedanti <140563395+omvedanti@users.noreply.github.com>
2023-07-27 13:20:48 +0000

    new file:   GNGC_CYBER_RANGE_WEBSITE/server.py

---
+++ GNGC_CYBER_RANGE_WEBSITE/server.py
@@ -0,0 +1,7 @@
+import sys
+import application
+
+#Here is where we gotta do something, its throwing an error because it needs a wsgi
+#https://www.youtube.com/watch?v=w0QDAg85Oow
+
+sys.path.insert 
