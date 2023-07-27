118a71800a1499af89e105d72a3680ec35b11322
Fox2low <77130209+Fox2low@users.noreply.github.com>
2023-07-27 02:03:33 +0000

    modified:   Maintance_Scripts/setupDB.sh

--- Maintance_Scripts/setupDB.sh
+++ Maintance_Scripts/setupDB.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# Function to set the root password
 set_root_password() {
     mysqladmin -u root password "GNGC-MYSQL"
     echo "Root password set successfully."
 }
 
+# Function to apply temporary MySQL configuration changes
+apply_mysql_config_fix() {
+    echo "Applying MySQL configuration fix..."
+    sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.backup
+    sudo sh -c "echo '[mysqld]' >> /etc/mysql/mysql.conf.d/mysqld.cnf"
+    sudo sh -c "echo 'skip-grant-tables' >> /etc/mysql/mysql.conf.d/mysqld.cnf"
+    sudo systemctl restart mysql
+}
+
+# Function to create a new user or update the password if the user already exists
 create_or_update_user() {
     local current_user=$(whoami)
     local new_password="pass"
@@ -22,5 +33,15 @@ MYSQL_SCRIPT
     echo "New account has been created or updated!"
 }
 
+# Function to revert MySQL configuration changes
+revert_mysql_config_fix() {
+    echo "Reverting MySQL configuration..."
+    sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf.backup /etc/mysql/mysql.conf.d/mysqld.cnf
+    sudo systemctl restart mysql
+}
+
+# Main script starts here
 set_root_password
+apply_mysql_config_fix
 create_or_update_user
+revert_mysql_config_fix
