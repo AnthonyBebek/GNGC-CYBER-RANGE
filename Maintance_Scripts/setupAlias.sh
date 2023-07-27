676d80f204a24be1335eb4194060752043e056b7
Fox2low <77130209+Fox2low@users.noreply.github.com>
2023-07-27 07:04:27 +0000

    new file:   Maintance_Scripts/setupAlias.sh 	renamed:    python-installs.bat -> setup.bat 	renamed:    python-installs.sh -> setup.sh

---
+++ Maintance_Scripts/setupAlias.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+NEW_ALIASES=(
+  "alias setup='~/GNGC-CYBER-RANGE/setup.sh'"
+  "alias update='cd ~ && git clone https://github.com/Fox2low/GNGC-CYBER-RANGE'"
+  "alias start='~/GNGC-CYBER-RANGE/start.sh'"
+  "alias start='~/GNGC-CYBER-RANGE/stop.sh'"
+)
+
+append_aliases() {
+  for alias_line in "${NEW_ALIASES[@]}"; do
+    if grep -qF "$alias_line" ~/.bashrc; then
+      echo "Alias already exists in ~/.bashrc: $alias_line"
+      echo "This is fine"
+    else
+      echo -e "\n# Custom Aliases" >> ~/.bashrc
+      echo "$alias_line" >> ~/.bashrc
+      echo "Alias added to ~/.bashrc: $alias_line"
+    fi
+  done
+
+  source ~/.bashrc
+}
+
+append_aliases
+
