ea010aebf81e1a5f317a2350b2a33e993cdbcf03
Fox2low <77130209+Fox2low@users.noreply.github.com>
2023-08-01 13:32:04 +1000

    Update setupAlias.sh

--- Maintance_Scripts/setupAlias.sh
+++ Maintance_Scripts/setupAlias.sh
@@ -2,7 +2,7 @@
 
 NEW_ALIASES=(
   "alias setup='~/GNGC-CYBER-RANGE/setup.sh'"
-  "alias update='cd ~ && git clone https://github.com/Fox2low/GNGC-CYBER-RANGE'"
+  "alias update='cd ~ && rm -r GNGC-CYBER-RANGE/ && git clone https://github.com/Fox2low/GNGC-CYBER-RANGE'"
   "alias start='~/GNGC-CYBER-RANGE/start.sh'"
   "alias stop='~/GNGC-CYBER-RANGE/stop.sh'"
   "alias restart='~/GNGC-CYBER-RANGE/stop.sh && ~/GNGC-CYBER-RANGE/start.sh"
