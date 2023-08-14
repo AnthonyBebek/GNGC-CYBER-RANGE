0e1e65088d9582c894e44a48666afa73cd07055e
Fox2low <77130209+Fox2low@users.noreply.github.com>
2023-08-14 06:22:43 +0000

    modified:   GNGC_CYBER_RANGE_SCRIPTS/temp.py

--- GNGC_CYBER_RANGE_SCRIPTS/temp.py
+++ GNGC_CYBER_RANGE_SCRIPTS/temp.py
@@ -1,5 +1,9 @@
 import challenge_info
 
-print(str(challenge_info.get_challenge_settings("Old School")))
+print("This is the 'get_challanges_settings' command")
+print("")
+print(str(challenge_info.get_challenge_settings("Generic Travel")))
+print("")
+print("This is the 'get_challanges' command")
 print("")
 print(str(challenge_info.get_challenges("Basic Command Line")))
